106131b2066752a2be0adfdbc741d6d51a0febaa
c-hadfield <chadfield@companieshouse.gov.uk>
2020-10-27 10:58:31 +0000

    Reformat line

--- groups/prometheus/module-prometheus/iam-role.tf
+++ groups/prometheus/module-prometheus/iam-role.tf
@@ -4,9 +4,7 @@ data "aws_iam_policy_document" "ec2_readonly" {
     actions = ["sts:AssumeRole"]
     principals {
         type = "Service"
-        identifiers = [
-            "ec2.amazonaws.com"
-        ]
+        identifiers = ["ec2.amazonaws.com"]
     }
     effect = "Allow"
   }
