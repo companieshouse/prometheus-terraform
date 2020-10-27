4987061ab60daee51a36a856efcaafc4ae6922d8
c-hadfield <chadfield@companieshouse.gov.uk>
2020-10-27 10:15:04 +0000

    Add iam-role instance profile

---
+++ groups/prometheus/module-prometheus/iam-role.tf
@@ -0,0 +1,28 @@
+data "aws_iam_policy_document" "ec2_readonly" {
+  statement {
+    # sid = "1"
+    actions = ["sts:AssumeRole"]
+    principals {
+        type = "Service"
+        identifiers = [
+            "ec2.amazonaws.com"
+        ]
+    }
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role" "ec2_readonly" {
+  name = "${var.environment}-${var.service}-ec2-read-only"
+  assume_role_policy = data.aws_iam_policy_document.ec2_readonly.json
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_readonly_attach" {
+  role       = aws_iam_role.ec2_readonly.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+}
+
+resource "aws_iam_instance_profile" "ec2_readonly" {
+    name = "${var.environment}-${var.service}-ec2-read-only"
+    role = aws_iam_role.ec2_readonly.name
+}
