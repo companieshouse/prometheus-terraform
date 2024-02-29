e9d2663a9ab2b8fafdcca54dd15d13976523127d
Jake Metherell <jake@fluffylogic.com>
2024-02-29 17:36:01 +0000

    kms alias needs to be unique for each environment

--- groups/prometheus/module-prometheus/main.tf
+++ groups/prometheus/module-prometheus/main.tf
@@ -1,7 +1,7 @@
 module "ssm_kms" {
   source                  = "git@github.com:companieshouse/terraform-modules//aws/kms?ref=tags/1.0.235"
-  kms_key_alias           = "${var.service}/ssm"
-  description             = "${var.service} parameter store use"
+  kms_key_alias           = "${var.service}-${var.environment}/ssm"
+  description             = "${var.service}-${var.environment} parameter store use"
   deletion_window_in_days = 30
   enable_key_rotation     = true
   is_enabled              = true
